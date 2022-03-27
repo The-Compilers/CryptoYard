acf2cd1296625aeba614675667b76bda13a6e8aa
PMolnes <pettermolnes@icloud.com>
2022-03-27 16:29:16 +0200

    Initialized the Spring Boot backend project.

---
+++ cryptoyard_backend/src/test/java/org/the_compilers/cryptoyard_backend/CryptoyardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.the_compilers.cryptoyard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoyardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
