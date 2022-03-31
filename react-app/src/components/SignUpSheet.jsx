705622abaa65901bcfad0c36b53c3e04b851ab8c
Torstein Eide <taurstein@gmail.com>
2022-03-31 08:32:59 +0200

    cleaned jsx file

--- react-app/src/components/SignUpSheet.jsx
+++ react-app/src/components/SignUpSheet.jsx
@@ -5,22 +5,21 @@ function SignUpSheet()
     return (
         <>
             <body>
-                <section class="sign-up-form__wrapper">
-                    <form class="sign-up-form" action="">
-                        <div class="sign-up-card">
+                
+                    <form class="sign-up-form box" action="">
+                        
                             <h3 class="sign-up-card__header">Create your account</h3>
-                            <input class="sign-up-card__element" type="text" placeholder="Username"/>
-                            <input class="sign-up-card__element" type="text" placeholder="Email" />
-                            <input class="sign-up-card__element" type="text" placeholder="Password"/>
-                            <input class="sign-up-card__element" type="text" placeholder="Repeat Password"/>
+                            <input class="sign-up-card__input" type="text" placeholder="Username"/>
+                            <input class="sign-up-card__input" type="text" placeholder="Email" />
+                            <input class="sign-up-card__input" type="text" placeholder="Password"/>
+                            <input class="sign-up-card__input" type="text" placeholder="Repeat Password"/>
                             <button class="sign-up-card__create-button" type="submit">Create Account</button>
                             <div class="sign-up-card__footer">
                                 <p>Already have an account?</p>
                                 <a class="sign-up-card__login" href="#">Log in</a>
                             </div>
-                        </div>
+                        
                     </form>
-                </section>
             </body>
         </>
     );
