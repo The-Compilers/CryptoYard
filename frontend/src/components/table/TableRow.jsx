68e0ec77636e7f41215d812d2ff5a9a11710ba60
jKm00 <joakied@stud.ntnu.no>
2022-04-05 20:30:33 +0200

    Refactored TableRow and removed unused dependency
    Now checks the values and adds the classes directly in the className attribute

--- frontend/src/components/table/TableRow.jsx
+++ frontend/src/components/table/TableRow.jsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-
 function CryptoTableRow({
   coin,
   balance,
@@ -8,57 +6,31 @@ function CryptoTableRow({
   lastDay,
   lastSevenHours,
 }) {
-  /**
-   * UseStates describing the state of the current value, wether it
-   * positiv or negative. If positiv, state is "success", if negative
-   * state if "error". States are appliied as css classes to display
-   * correct color
-   */
-  const [profitState, setProfitState] = useState("");
-  const [lastDayState, setLastDayState] = useState("");
-  const [lastSevenHoursState, setLastSevenHoursState] = useState("");
-
-  /**
-   * Checks the values if they are positiv or negative.
-   * If positiv set color of text to green, if negative
-   * set color of text to red.
-   * @param {*} value the value to check
-   * @param {*} updater the useState needed to be updated to
-   * apply class to correct row with correct color
-   */
-  function checkValue(value, updater) {
-    const firstLetter = value.charAt(0);
-    if (firstLetter == "+") {
-      updater("success");
-    } else if (firstLetter == "-") {
-      updater("error");
-    } else {
-      updater("");
-    }
-  }
-
-  useEffect(() => {
-    checkValue(profit, setProfitState);
-    checkValue(lastDay, setLastDayState);
-    checkValue(lastSevenHours, setLastSevenHoursState);
-  });
-
   return (
     <tr className="table__row">
       <td className="table__column">{coin}</td>
       <td className="table__column table__column--align-right">${balance}</td>
-      <td className={"table__column table__column--align-right " + profitState}>
+      <td
+        className={
+          "table__column table__column--align-right " +
+          (parseFloat(profit) > 0 ? "success" : "error")
+        }
+      >
         {profit.charAt(0) + "$" + profit.substring(1)}
       </td>
       <td className="table__column table__column--align-right">${price}</td>
       <td
-        className={"table__column table__column--align-right " + lastDayState}
+        className={
+          "table__column table__column--align-right " +
+          (parseFloat(lastDay) > 0 ? "success" : "error")
+        }
       >
         {lastDay}%
       </td>
       <td
         className={
-          "table__column table__column--align-right " + lastSevenHoursState
+          "table__column table__column--align-right " +
+          (parseFloat(lastSevenHours) > 0 ? "success" : "error")
         }
       >
         {lastSevenHours}%
