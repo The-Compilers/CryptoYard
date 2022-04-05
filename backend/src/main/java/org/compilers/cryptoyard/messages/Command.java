22912e79660d228ac96a15f7ba1b20838af7c167
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-05 07:28:11 +0200

    (WIP) Some base classes for messaging

---
+++ backend/src/main/java/org/compilers/cryptoyard/messages/Command.java
@@ -0,0 +1,8 @@
+package org.compilers.cryptoyard.messages;
+
+/**
+ * A message which commands the receiver to do a specific action.
+ * Use this when it is quite clear how the receiver should react.
+ */
+public class Command extends Message {
+}
---
+++ backend/src/main/java/org/compilers/cryptoyard/messages/MessageDispatcher.java
@@ -0,0 +1,10 @@
+package org.compilers.cryptoyard.messages;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Responsible for managing subscriptions and forwarding messages to necessary services.
+ */
+@Component
+public class MessageDispatcher {
+}
