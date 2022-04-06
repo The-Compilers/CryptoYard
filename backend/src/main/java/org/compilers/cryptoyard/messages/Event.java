fbdb0a8f9eb7fb0ec66a761d9c673795cbf91518
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-06 22:34:52 +0200

    Command and Event classes made abstract

--- backend/src/main/java/org/compilers/cryptoyard/messages/Event.java
+++ backend/src/main/java/org/compilers/cryptoyard/messages/Event.java
@@ -4,5 +4,5 @@ package org.compilers.cryptoyard.messages;
  * A message which signals an event (without specifying how the listeners should react)
  * Use this for events which can be perceived differently by different receivers.
  */
-public class Event extends Message {
+public abstract class Event extends Message {
 }
