ff081a4bc8ac55dcfaa78ce0d0b4628f05a1959b
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-08 20:30:21 +0200

    Delete unnecessary classes

--- backend/src/main/java/org/compilers/cryptoyard/exchange/api/BinanceRestApiClient.java
+++
@@ -1,8 +0,0 @@
-package org.compilers.cryptoyard.exchange.api;
-
-/**
- * Communicates with Binance REST API
- */
-public class BinanceRestApiClient {
-    // TODO
-}
--- backend/src/main/java/org/compilers/cryptoyard/exchange/api/BinanceWebsocketClient.java
+++
@@ -1,8 +0,0 @@
-package org.compilers.cryptoyard.exchange.api;
-
-/**
- * Communicates with Binance WebSocket API
- */
-public class BinanceWebsocketClient {
-    // TODO
-}
