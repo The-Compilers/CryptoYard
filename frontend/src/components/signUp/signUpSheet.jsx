0e77d04867fa906e0c11f00a77a97a47b2828c77
Girts <girts.strazdins@gmail.com>
2022-04-28 18:02:11 +0200

    (WIP) merging styles for sign-up and sign-in forms, not done yet

--- frontend/src/components/signUp/signUpSheet.jsx
+++ frontend/src/components/signUp/signUpSheet.jsx
@@ -7,20 +7,22 @@ function SignUpSheet() {
     const navigate = useNavigate();
 
     return (
-        <form className="sign-up-form box" action="">
+        <main className="sign-up-form__background">
+            <form className="sign-up-form box" action="">
 
-            <h3 className="sign-up-form__header">Create your account</h3>
-            <input className="sign-up-form__input" type="text" placeholder="Username"/>
-            <input className="sign-up-form__input" type="text" placeholder="Email"/>
-            <input className="sign-up-form__input" type="text" placeholder="Password"/>
-            <input className="sign-up-form__input" type="text" placeholder="Repeat Password"/>
-            <button className="sign-up-form__create-button" type="submit">Create Account</button>
-            <div className="sign-up-form__footer">
-                <p>Already have an account?</p>
-                <a onClick={() => (navigate("/signin"))} className="sign-up-form__login" href="#">Log in</a>
-            </div>
+                <h3 className="sign-up-form__header">Create your account</h3>
+                <input className="sign-up-form__input" type="text" placeholder="Username"/>
+                <input className="sign-up-form__input" type="text" placeholder="Email"/>
+                <input className="sign-up-form__input" type="text" placeholder="Password"/>
+                <input className="sign-up-form__input" type="text" placeholder="Repeat Password"/>
+                <button className="sign-up-form__create-button" type="submit">Create Account</button>
+                <div className="sign-up-form__footer">
+                    <p>Already have an account?</p>
+                    <a onClick={() => (navigate("/signin"))} className="sign-up-form__login" href="#">Log in</a>
+                </div>
 
-        </form>
+            </form>
+        </main>
     );
 }
 
