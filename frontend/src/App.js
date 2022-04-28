fc3e783af5d178e2676adf22b5ebf2343362617f
Girts <girts.strazdins@gmail.com>
2022-04-28 17:54:54 +0200

    Update to React v18, cleanup
    Rename sign-up-card_... classes to sign-up-form_... for consistency
    Update accent colors according to design guideline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import "./styles/global.css";
 
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import SignUpSheet from "./components/signUp/signUpSheet";
 import SignInSheet from "./components/signIn/SignInSheet";
 
 
 function App() {
-  return (
-      <div>
-          <Routes>
-              <Route path ="/signin" element = {<SignInSheet />}/>
-              <Route path ="/signup" element = {<SignUpSheet />}/>
-          </Routes>
-      </div>
-  );
+    return (
+        <Routes>
+            <Route path="/signin" element={<SignInSheet/>}/>
+            <Route path="/signup" element={<SignUpSheet/>}/>
+        </Routes>
+    );
 }
 
 export default App;
--- frontend/src/components/signUp/signUpSheet.jsx
+++ frontend/src/components/signUp/signUpSheet.jsx
@@ -2,31 +2,25 @@ import "../signUp/signUp.css"
 import {useNavigate} from "react-router";
 
 
-function SignUpSheet()
-{
+function SignUpSheet() {
 
     const navigate = useNavigate();
 
     return (
-        <>
-            <body>
+        <form className="sign-up-form box" action="">
 
-                    <form className="sign-up-form box" action="">
+            <h3 className="sign-up-form__header">Create your account</h3>
+            <input className="sign-up-form__input" type="text" placeholder="Username"/>
+            <input className="sign-up-form__input" type="text" placeholder="Email"/>
+            <input className="sign-up-form__input" type="text" placeholder="Password"/>
+            <input className="sign-up-form__input" type="text" placeholder="Repeat Password"/>
+            <button className="sign-up-form__create-button" type="submit">Create Account</button>
+            <div className="sign-up-form__footer">
+                <p>Already have an account?</p>
+                <a onClick={() => (navigate("/signin"))} className="sign-up-form__login" href="#">Log in</a>
+            </div>
 
-                            <h3 className="sign-up-card__header">Create your account</h3>
-                            <input className="sign-up-card__input" type="text" placeholder="Username"/>
-                            <input className="sign-up-card__input" type="text" placeholder="Email" />
-                            <input className="sign-up-card__input" type="text" placeholder="Password"/>
-                            <input className="sign-up-card__input" type="text" placeholder="Repeat Password"/>
-                            <button className="sign-up-card__create-button" type="submit">Create Account</button>
-                            <div className="sign-up-card__footer">
-                                <p>Already have an account?</p>
-                                <a onClick={() => (navigate("/signin"))} className="sign-up-card__login" href="#">Log in</a>
-                            </div>
-
-                    </form>
-            </body>
-        </>
+        </form>
     );
 }
 
