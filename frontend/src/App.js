64cde7f9bfec9df6df4e2eed53b2e1e2d1e73cc1
Joakim <joakied@stud.ntnu.no>
2022-10-04 11:22:16 +0200

    404 not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 // Routing
-import {Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
 // Global styles
 import "./styles/global.css";
 
 // Pages
 import Dashboard from "./pages/dashboard/Dashboard";
-import {SignInUpForm} from "./pages/signin-signup/SignInUpForm";
+import { SignInUpForm } from "./pages/signin-signup/SignInUpForm";
 import Market from "./pages/market/Market";
 
 // Services
-import {UserContext} from "./state/UserContext";
-import {deleteAuthorizationCookies, getAuthenticatedUser} from "./services/authentication";
-import {useState} from "react";
+import { UserContext } from "./state/UserContext";
+import {
+  deleteAuthorizationCookies,
+  getAuthenticatedUser,
+} from "./services/authentication";
+import { useState } from "react";
 import Nav from "./components/nav/Nav";
+import NotFound from "./pages/404/NotFound";
 
 /**
  * The main application component
@@ -22,10 +26,12 @@ import Nav from "./components/nav/Nav";
  */
 function App() {
   let [user, setUser] = useState(getAuthenticatedUser());
+  const navigate = useNavigate();
 
   function onSignInSuccess(userData) {
     console.log("User authenticated!");
     setUser(userData);
+    navigate("/dashboard");
   }
 
   function onSignUpSuccess(userData) {
@@ -44,23 +50,32 @@ function App() {
 
   return (
     <UserContext.Provider value={user}>
-      {
-        user ?
-          <>
-            <Nav onLogOut={handleLogOut}/>
-            <Routes>
-              <Route path="/dashboard/" element={<Dashboard/>}/>
-              <Route path="/markets" element={<Market/>}/>
-              <Route path="*" element={<Navigate to="/dashboard"/>}/>
-            </Routes>
-          </>
-          :
+      {user ? (
+        <>
+          <Nav onLogOut={handleLogOut} />
           <Routes>
-            <Route path="/signin" element={<SignInUpForm isSignIn={true} onSuccess={onSignInSuccess}/>}/>
-            <Route path="/signup" element={<SignInUpForm isSignIn={false} onSuccess={onSignUpSuccess}/>}/>
-            <Route path="*" element={<Navigate to="/signin"/>}/>
+            <Route path="/dashboard/" element={<Dashboard />} />
+            <Route path="/markets" element={<Market />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
-      }
+        </>
+      ) : (
+        <Routes>
+          <Route
+            path="/signin"
+            element={
+              <SignInUpForm isSignIn={true} onSuccess={onSignInSuccess} />
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <SignInUpForm isSignIn={false} onSuccess={onSignUpSuccess} />
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      )}
     </UserContext.Provider>
   );
 }
